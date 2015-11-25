2c4d68aa9539c370195f05f3bea4f41b7adcdd9e
Jérémie Litzler <contact@jeremielitzler.net>
2015-11-25 14:03:52 +0000

    Make the Project configurable

--- mysql_configure.sh
+++ mysql_configure.sh
@@ -1,7 +1,8 @@
 echo "Setting password for mysql root user..."
 mysql .\ mysql_password_change.sql
 echo "Executing the database initialization..."
-DB_INIT_SH="EasyMvc/Database/Scripts"
+PROJECT_NAME="EasyMvc"
+DB_INIT_SH=$PROJECT_NAME"/Database/Scripts"
 echo "Database shell script is found in "$DB_INIT_SH
 chmod $DB_INIT_SH/db_init_on_server.sh
 sh $DB_INIT_SH/db_init_on_server.sh
