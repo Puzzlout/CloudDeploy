633b52b44494e2f468c0039bd7ddf3e8a40c78e8
Jérémie Litzler <contact@jeremielitzler.net>
2015-12-24 07:14:02 +0000

    refining the script to display all commands to run

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,5 @@ mysql-ctl start
 echo "Mysql server launched."
 git clone https://github.com/$1/$2.git
 echo "Configuring MySQL and setting up the project database..."
-echo "Go to https://remotecoding-webdevjl.c9users.io/phpmyadmin and run the mysql_password_change.sql"
-echo "Then run "$2"/Database/Scripts/db_reset.sql"
-#chmod +x CloudDeploymentManager/mysql_configure.sh
-#sh CloudDeploymentManager/mysql_configure.sh $1 $2
+chmod +x CloudDeploymentManager/mysql_configure.sh
+sh CloudDeploymentManager/mysql_configure.sh $1 $2
--- mysql_configure.sh
+++ mysql_configure.sh
@@ -2,10 +2,10 @@
 #$2 => the application name to load and configure
 #echo "Setting password for mysql root user..."
 #mysql SET @mysql_user = $1; \. CloudDeploymentManager/mysql_password_change.sql
-echo "Executing the database initialization..."
-echo "... for project "$2
-DB_INIT_FOLDER=$2"/Database/Scripts/"
-DB_INIT_SH=$DB_INIT_FOLDER"db_init_on_server.sh"
+#echo "Executing the database initialization..."
+#echo "... for project "$2
+#DB_INIT_FOLDER=$2"/Database/Scripts/"
+#DB_INIT_SH=$DB_INIT_FOLDER"db_init_on_server.sh"
 echo "Database shell script is found in shell script: "$DB_INIT_SH
 chmod +x $DB_INIT_SH
 sh $DB_INIT_SH $1 $DB_INIT_FOLDER
