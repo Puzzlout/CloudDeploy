0233cbadfb9c4592db2865cf04195a6a977177b0
Jérémie Litzler <contact@jeremielitzler.net>
2016-02-09 12:58:31 +0000

    php-upgrade.sh update

--- php-upgrade.sh
+++ php-upgrade.sh
@@ -12,6 +12,7 @@ phpbrew update
 # Avoid 5.6.16 ==> is crashing!
 phpbrew --debug install 5.6.10 as 5.6-dev +default
 source ~/.phpbrew/bashrc
+phpbrew lookup-prefix homebrew
 phpbrew switch php-5.6.10
 phpbrew use 5.6-dev
 php -v
