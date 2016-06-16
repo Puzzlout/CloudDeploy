9ff6d6f1cb3a3bf5c0cea394ca68bcf23da18576
Stephane Charoy <trektours.pm@gmail.com>
2016-06-16 10:43:02 +0000

    TrekTours C9 installer

--- Projects/TrekTours/install.sh
+++ Projects/TrekTours/install.sh
@@ -1,5 +1,16 @@
-git clone https://github.com/Puzzlout/TrekToursFlyer.git
-cd TrekToursFlyer
+#$1 dev or prod
+git pull
+echo "Run composer..."
 composer install
 composer update
+npm install -g bower
+echo "Run bower..."
+bower install
+bower update
+echo "IMPORTANT: Require a mysql root user with no password"
+php bin/console doctrine:database:create --if-not-exists
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:schema:update --dump-sql
+php bin/console doctrine:schema:update --force
+php bin/console assetic:dump --env=$1 --no-debug
 php bin/console asset:install --symlink
--- Projects/TrekTours/refresh.sh
+++ Projects/TrekTours/refresh.sh
@@ -2,6 +2,11 @@
 rm -R var/cache
 rm -R var/sessions
 rm var/bootstrap.php.cache
+git pull
 composer update
+bower update
 php bin/console cache:clear --env=$1
+php bin/console doctrine:schema:update --dump-sql
+php bin/console doctrine:schema:update --force
+php bin/console assetic:dump --env=$1 --no-debug
 php bin/console asset:install --symlink
