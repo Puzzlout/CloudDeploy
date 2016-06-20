3f9f613d8672abb44a5c375324b980326dfc4c17
Stephane Charoy <trektours.pm@gmail.com>
2016-06-20 08:55:42 +0000

    TrekTours installers update

---
+++ Projects/TrekTours/Api/refresh.sh
@@ -0,0 +1,11 @@
+#$1 dev or prod
+rm -R var/cache
+rm -R var/sessions
+rm var/bootstrap.php.cache
+git pull
+composer update
+bower update
+php bin/console cache:clear --env=$1
+php bin/console doctrine:schema:update --dump-sql
+php bin/console doctrine:schema:update --force
+php bin/console asset:install --symlink
