c813eb287d5508bef0742581b20df9b8bea046fb
Puzzlout team <contact@puzzlout.com>
2016-07-08 06:13:37 +0000

    #1

---
+++ Projects/TrekTours/init.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Puzzlout/CloudDeploy.git
+mv CloudDeploy deploy
+git clone https://github.com/Puzzlout/TrekToursFlyer.git
+mv TrekToursFlyer public
+git clone https://github.com/Puzzlout/TrekToursApi.git
+mv TrekToursApi api
