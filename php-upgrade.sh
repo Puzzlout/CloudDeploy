ce188e96721b4b96efb886b47118091307ccf1a4
Damir <damirius@gmail.com>
2016-07-12 13:24:39 +0200

    #1 php_upgrade change

--- php-upgrade.sh
+++ php-upgrade.sh
@@ -17,14 +17,14 @@ sudo chmod oga+rw /usr/lib/apache2/modules
 sudo chmod -R oga+rw /etc/apache2/
 sudo chmod -R oga+rw /var/lib/apache2/module/enabled_by_admin
 phpbrew -d install $1 +default +mysql +apxs2
-sudo chmod og-w /usr/lib/apache2/modules
-sudo chmod -R og-w /etc/apache2/
-sudo chmod -R oga-w /var/lib/apache2/module/enabled_by_admin
 source ~/.phpbrew/bashrc
-phpbrew switch $1
-phpbrew use $1
+phpbrew switch php-$1
+phpbrew use php-$1
 phpbrew ext install curl
 cp /etc/apache2/mods-available/php5.conf /etc/apache2/mods-available/php7.conf
 ln -s /etc/apache2/mods-available/php7.conf /etc/apache2/mods-enabled/php7.conf
 rm /etc/apache2/mods-enabled/php5.load
+sudo chmod og-w /usr/lib/apache2/modules
+sudo chmod -R og-w /etc/apache2/
+sudo chmod -R oga-w /var/lib/apache2/module/enabled_by_admin
 php -v
