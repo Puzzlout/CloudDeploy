a2f5195e496800d308cb3a48d44b235ae73f5593
Jérémie Litzler <contact@jeremielitzler.net>
2016-07-18 22:11:32 +0200

    #1
    local installer for MACOSX

---
+++ Projects/TrekTours/Api/install-locally-mac.sh
@@ -0,0 +1,14 @@
+#$1 dev or prod
+cd /www/sites/api/
+git pull
+echo "Run composer..."
+composer install
+composer update
+echo "IMPORTANT: Require a mysql root user with no password"
+php bin/console doctrine:database:create --if-not-exists
+php bin/console doctrine:schema:update --dump-sql
+php bin/console doctrine:schema:update --force
+echo "IMPORTANT: Admin user created with user admin, pass admin"
+php bin/console fos:user:create admin admin@admin.com admin --super-admin
+php bin/console assets:install --env=$1 --symlink
+php bin/console cache:warmup --env=$1
---
+++ Projects/TrekTours/Api/refresh-locally-mac.sh
@@ -0,0 +1,13 @@
+#$1 dev or prod
+cd /www/sites/api/
+rm -R var/cache
+rm -R var/sessions
+rm var/bootstrap.php.cache
+git pull
+mysql-ctl start
+composer update
+php bin/console cache:clear --env=$1
+php bin/console doctrine:schema:update --dump-sql
+php bin/console doctrine:schema:update --force
+php bin/console assets:install --env=$1 --symlink
+php bin/console cache:warmup --env=$1
