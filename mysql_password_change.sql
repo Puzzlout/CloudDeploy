3d92d7a1c0aeccd54a5c8277a18e5f6e40ffd7ce
Jérémie Litzler <contact@puzzlout.com>
2016-07-23 22:31:49 +0200

    Delete mysql_password_change.sql

--- mysql_password_change.sql
+++
@@ -1,7 +0,0 @@
-SET PASSWORD FOR 'webdevjl'@'%' = PASSWORD('jUL%C9%15');
-
-## Sets the password for @mysql_user
-#SET @query = CONCAT('SET PASSWORD FOR ''webdevjl''@''%'' = PASSWORD(''jUL%C9%15'');');
-#PREPARE stmt FROM @query;
-#EXECUTE stmt;
-#DEALLOCATE PREPARE stmt;
