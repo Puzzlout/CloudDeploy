29b60070106db99f97b2c75c54d9d55d0a2e405f
Jérémie Litzler <contact@jeremielitzler.net>
2016-08-17 19:32:01 +0200

    #2

--- Projects/TrekTours/Api/update.sh
+++ Projects/TrekTours/Api/update.sh
@@ -13,5 +13,4 @@ php bin/console cache:clear --env=$1
 php bin/console doctrine:schema:update --dump-sql
 php bin/console doctrine:schema:update --force
 php bin/console assets:install --env=$1 --symlink
-php bin/console assetic:dump --env=$1 --no-debug
 php bin/console cache:warmup --env=$1
