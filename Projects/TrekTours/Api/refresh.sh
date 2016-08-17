b698bcc789ce42c52a3d713200fadf0575a60189
Jérémie Litzler <contact@jeremielitzler.net>
2016-08-17 06:55:50 +0200

    #4

--- Projects/TrekTours/Api/refresh.sh
+++ Projects/TrekTours/Api/refresh.sh
@@ -5,7 +5,6 @@ if [ ! -d "$2" ]; then
 fi
 cd $2
 php bin/console cache:clear --env=$1
-php bin/console lexik:translations:import AppBundle --cache-clear
 php bin/console assets:install --env=$1 --symlink
 php bin/console assetic:dump --env=$1 --no-debug
 php bin/console cache:warmup --env=$1
