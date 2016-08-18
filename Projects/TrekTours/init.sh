c18f33d7e3a84d10f74a39049736a557b8d19b03
Jérémie Litzler <contact@puzzlout.com>
2016-08-18 06:55:47 +0200

    #5

--- Projects/TrekTours/init.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/Puzzlout/CloudDeploy.git
-mv CloudDeploy deploy
-git clone https://github.com/Puzzlout/TrekToursFlyer.git
-mv TrekToursFlyer public
-git clone https://github.com/Puzzlout/TrekToursApi.git
-mv TrekToursApi api
