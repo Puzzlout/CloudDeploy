c982f8135c187b84928dd4d5f06999c0656dc9da
Jérémie Litzler <contact@jeremielitzler.net>
2016-09-02 06:44:13 +0200

    refresh scripts tweak

--- Projects/TrekTours/Api/refresh.sh
+++ Projects/TrekTours/Api/refresh.sh
@@ -3,7 +3,7 @@ if [ ! -d "$2" ]; then
   echo "Directory "$2" doesn't exits!"
   exit
 fi
-cd $2
+cd $2/api
 php bin/console cache:clear --env=$1
 php bin/console assets:install --env=$1 --symlink
 php bin/console assetic:dump --env=$1 --no-debug
--- Projects/TrekTours/Flyer/refresh.sh
+++ Projects/TrekTours/Flyer/refresh.sh
@@ -3,7 +3,7 @@ if [ ! -d "$2" ]; then
   echo "Directory "$2" doesn't exits!"
   exit
 fi
-cd $2
+cd $2/public
 php bin/console cache:clear --env=$1
 php bin/console lexik:translations:import AppBundle --cache-clear
 php bin/console assets:install --env=$1 --symlink
